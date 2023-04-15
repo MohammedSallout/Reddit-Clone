82fa2303a8a4fadbcb0aa49df505e3900ecbe856
MohammedSallout <msallout294@gmail.com>
2023-04-15 18:28:34 +0300

    handle client side validation Relates #5

---
+++ public/validation/signup.js
@@ -0,0 +1,39 @@
+const signupForm = document.querySelector('#signup-form')
+const validUsername = document.querySelector('.valid-username')
+const validEmail = document.querySelector('.valid-email')
+const validPassword = document.querySelector('.valid-password')
+const success = document.querySelector('.success')
+const username = document.querySelector('#username')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if (username.value === '') {
+    validUsername.textContent = 'Username is required*'
+  } else if (username.value.length >= 20) {
+    validUsername.textContent = 'Maximum size is 20*'
+  } else if (email.value === '') {
+    validUsername.textContent = ''
+    validEmail.textContent = 'Email is required*'
+  } else if (!email.value.split('').includes('.', '@')) {
+    validUsername.textContent = ''
+    validEmail.textContent = 'Wrong email value*'
+  } else if (email.value.length >= 20) {
+    validUsername.textContent = ''
+    validEmail.textContent = 'Maximum size is 20*'
+  } else if (password.value === '') {
+    validEmail.textContent = ''
+    validPassword.textContent = 'Password is required*'
+  } else if (password.value.length < 6) {
+    validEmail.textContent = ''
+    validPassword.textContent = 'Minimum size is 6*'
+  } else if (password.value.length > 15) {
+    validEmail.textContent = ''
+    validPassword.textContent = 'Maximum size is 15*'
+  } else {
+    validPassword.textContent = ''
+    window.location.href = '/html/signup.html'
+    success.textContent = 'Sign Up Successfully'
+  }
+})
